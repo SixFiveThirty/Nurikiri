401637005b16ea282381d57a725ff905e921b089
Oh Ju Seok <jusug2080@naver.com>
2023-11-04 15:16:52 +0900

    네!

--- src/main/java/oh/ju/seok/master2.java
+++ src/main/java/oh/ju/seok/master2.java
@@ -1,10 +1,10 @@
 package oh.ju.seok;
 
-public class master {
+public class master2 {
 
 	public static void main(String[] args) {
 
-		System.out.println("과연 그럴까요?");
+		System.out.println("내!!");
 
 	}
 }
