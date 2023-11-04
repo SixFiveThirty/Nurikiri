08a546b2b7c78dcc6ff0b7874ce8967867da6e2c
Oh Ju Seok <jusug2080@naver.com>
2023-11-04 15:11:58 +0900

    이제 개인 설정 커밋하라고 안뜹니다.

---
+++ src/main/java/oh/ju/seok/OHI4.java
@@ -0,0 +1,10 @@
+package oh.ju.seok;
+
+public class OHI4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("이제 개인 설정 커밋하라는 내용이 안뜨네요!");
+	}
+
+}
