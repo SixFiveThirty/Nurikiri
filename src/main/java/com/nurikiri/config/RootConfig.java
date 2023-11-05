a57b32f577d1744593259f2ef26176d61366b174
Jeon Hayoon <kjklove0701@gmail.com>
2023-11-05 19:02:59 +0900

    web.xml, root-context.xml, servlet-context.xml 삭제, RootConfig.java, ServletConfig.java, WebConfig.java 생성 (Java 설정으로 변경)

---
+++ src/main/java/com/nurikiri/config/RootConfig.java
@@ -0,0 +1,36 @@
+package com.nurikiri.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@MapperScan(basePackages = {"com.nurikiri.mapper"})
+public class RootConfig {
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig config = new HikariConfig();
+		config.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		config.setJdbcUrl("jdbc:log4jdbc:mysql://localhost:3306/nurikiri");
+		config.setUsername("nurikiri");
+		config.setPassword("nurikiri1234");
+		
+		HikariDataSource dataSource = new HikariDataSource(config);
+		
+		return dataSource;
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource());
+		return (SqlSessionFactory) sqlSessionFactory.getObject();
+	}
+}
