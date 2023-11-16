45d3b2aab31e43d99d9a285a68c178cd5e86d63f
Chang Ha Yeong <bestdev.hy@gmail.com>
2023-11-16 09:11:39 +0900

    Revert "StoreAdminCotroller, VO, ServiceImple 커밋"
    This reverts commit bcad3e2df1f8ecd1d6b5a8550d48dee2f43e312f.

--- src/main/java/com/nurikiri/service/StoreAdminServiceImpl.java
+++ src/main/java/com/nurikiri/service/StoreAdminServiceImpl.java
@@ -3,16 +3,21 @@ package com.nurikiri.service;
 import java.util.List;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import com.nurikiri.domain.StoreAdminDAO;
 import com.nurikiri.domain.StoreAdminVO;
 import com.nurikiri.mapper.StoreAdminMapper;
 
+import lombok.RequiredArgsConstructor;
 
 @Service
 public class StoreAdminServiceImpl implements StoreAdminService{
 	
-	
+	@Autowired
+	private StoreAdminDAO dao;
 	
 	@Override
 	public List<StoreAdminVO> storeAdminList() throws Exception{
