c5bac9e334fccf46cc69fdc892a0ed51fbe1f482
강우성 <141286813+kwssssss@users.noreply.github.com>
2023-11-22 15:41:02 +0900

    검색기능 구현중

--- src/main/java/com/nurikiri/controller/SearchController.java
+++ src/main/java/com/nurikiri/controller/SearchController.java
@@ -1,5 +1,24 @@
 package com.nurikiri.controller;
 
-public class SearchController {
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value ="/search")
+public class SearchController {
+	
+	@ModelAttribute("searchTypes")
+	public Map<String, String> searchTypes() {
+		Map<String, String> map = new LinkedHashMap<String, String>();
+		map.put("", "별점순");
+		map.put("", "즐겨찾기순");
+		map.put("", "리뷰순");
+		map.put("", "지역별");
+		
+		return map;
+	}
 }
