5456374285faec3fe88c6bb5841b8a42202d6cf1
JAmIN <flf111@naver.com>
2023-11-28 13:27:12 +0900

    231128(화) 즐겨찾기 기능 및 가맹점 이미지 경로 설정. (EditorServiceImpl, StoreServiceImple에서 THUMBNAIL_UPLOAD_DIR 경로를 개인 로컬폴더 경로로 수정하시면 됩니다.)

--- src/main/java/com/nurikiri/mapper/StoreMapper.java
+++ src/main/java/com/nurikiri/mapper/StoreMapper.java
@@ -2,27 +2,31 @@ package com.nurikiri.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Select;
-
 import com.nurikiri.domain.Criteria;
+import com.nurikiri.domain.StoreBookmarkVO;
 import com.nurikiri.domain.StoreVO;
 
 public interface StoreMapper {
 
 	public List<StoreVO> getList();
-	
+
 	public void insert(StoreVO store);
-	
+
 	public int update(StoreVO store);
-	
+
 	public int delete(Long sno);
-	
+
 	public StoreVO read(Long sno);
-	
+
 	public List<StoreVO> getListWithPaging(Criteria cri);
-	
+
 	public int getTotalCount(Criteria cri);
 
-	
-	
+	// 즐겨찾기 처리
+	public List<Long> getStoreBookmarksList(String username);
+
+	public int addStoreBookmark(StoreBookmarkVO storeBookmark);
+
+	public int deleteStoreBookmark(StoreBookmarkVO storeBookmark);
+
 }
